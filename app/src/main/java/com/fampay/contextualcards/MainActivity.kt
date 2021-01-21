686ca2c7fa00466609e213b636ef6fc15668c231
webianks <webianks@gmail.com>
2021-01-21 12:37:25 +0530

    Add main viewmodel for handling data request and response

--- app/src/main/java/com/fampay/contextualcards/MainActivity.kt
+++ app/src/main/java/com/fampay/contextualcards/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.fampay.contextualcards
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.fampay.contextualcards.data.network.Networking
+import com.fampay.contextualcards.util.ViewModelProviderFactory
+import io.reactivex.disposables.CompositeDisposable
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainViewModel = ViewModelProvider(this, ViewModelProviderFactory(MainViewModel::class) {
+            MainViewModel(
+                    CompositeDisposable(), Networking.create(
+                    BuildConfig.BASE_URL,
+                    application.cacheDir,
+                    5 * 1024 * 1024
+            )
+            )
+        }).get(MainViewModel::class.java)
+
+        mainViewModel.getCardGroups()
+
+        mainViewModel.cardGroupResponseData.observe(this) {
+            Log.i(TAG, "CardGroupResponse count ${it.cardGroups.size}")
+
+        }
     }
 }
---
+++ app/src/main/java/com/fampay/contextualcards/MainViewModel.kt
@@ -0,0 +1,38 @@
+package com.fampay.contextualcards
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.fampay.contextualcards.data.network.NetworkService
+import com.fampay.contextualcards.data.network.response.CardGroupResponse
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class MainViewModel(
+    private val compositeDisposable: CompositeDisposable,
+    private val networkService: NetworkService
+) : ViewModel() {
+
+    val cardGroupResponseData = MutableLiveData<CardGroupResponse>()
+
+    companion object{
+        const val TAG = "MainViewModel"
+    }
+    fun getCardGroups() {
+        compositeDisposable.addAll(
+            networkService.doGetCardGroupsCall()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    cardGroupResponseData.postValue(it)
+                },{
+                    Log.e(TAG,"Something went wrong: ${it.message}")
+                }))
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
+}
