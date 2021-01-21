5ffd788d133d86625eec5f19b13ebcda92860c34
webianks <webianks@gmail.com>
2021-01-21 13:44:51 +0530

    Add logic for showing HC1 cards [scrollable/non-scrollable]

---
+++ app/src/main/java/com/fampay/contextualcards/ui/HorizontalRecyclerViewAdapter.kt
@@ -0,0 +1,95 @@
+package com.fampay.contextualcards.ui
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.fampay.contextualcards.R
+import com.fampay.contextualcards.data.network.response.Card
+import com.fampay.contextualcards.ui.MainRecyclerViewAdapter.Companion.HC1
+import com.fampay.contextualcards.ui.MainRecyclerViewAdapter.Companion.HC9
+import com.google.android.material.card.MaterialCardView
+import kotlinx.android.synthetic.main.item_dynamic_width.view.*
+import kotlinx.android.synthetic.main.item_view_small_display.view.*
+
+class HorizontalRecyclerViewAdapter(
+    private val context: Context,
+    val list: List<Card>,
+    val type: Int
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    var mRecyclerView: RecyclerView? = null
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        mRecyclerView = recyclerView
+    }
+
+    private inner class HorizontalViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(position: Int) {
+
+            val card = list[position]
+
+            if (type == HC1) {
+
+                itemView.tv_title.text = card.formattedTitle.text
+                if (card.formattedDescription != null) {
+                    itemView.tv_description.text = card.formattedDescription.text
+                    itemView.tv_description.visibility = View.VISIBLE
+                } else
+                    itemView.tv_description.visibility = View.GONE
+
+                Glide.with(itemView.context)
+                    .load(card.icon.imgUrl)
+                    .placeholder(R.drawable.default_icon_background)
+                    .error(R.drawable.default_icon_background)
+                    .into(itemView.iv_icon)
+
+                if (itemView is MaterialCardView && card.bgColor != null)
+                    itemView.setCardBackgroundColor(Color.parseColor(card.bgColor))
+
+            } else {
+                val layoutParams = itemView.layoutParams
+                layoutParams.width =
+                    (layoutParams.height / card.backgroundImage.aspectRatio).toInt()
+                Glide.with(itemView.context).load(card.backgroundImage.imgUrl)
+                    .into(itemView.iv_image_dynamic)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (type) {
+            HC1 -> {
+                HorizontalViewHolder(
+                    LayoutInflater.from(context)
+                        .inflate(R.layout.item_view_small_display, parent, false)
+                )
+            }
+            HC9 -> {
+                HorizontalViewHolder(
+                    LayoutInflater.from(context)
+                        .inflate(R.layout.item_dynamic_width, parent, false)
+                )
+            }
+            else -> {
+                HorizontalViewHolder(
+                    LayoutInflater.from(context)
+                        .inflate(R.layout.item_dynamic_width, parent, false)
+                )
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as HorizontalViewHolder).bind(position)
+    }
+}
--- app/src/main/java/com/fampay/contextualcards/ui/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/fampay/contextualcards/ui/MainRecyclerViewAdapter.kt
@@ -1,13 +1,19 @@
 package com.fampay.contextualcards.ui
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.fampay.contextualcards.R
 import com.fampay.contextualcards.data.network.response.CardGroup
+import com.fampay.contextualcards.util.px
+import com.google.android.material.card.MaterialCardView
+import kotlinx.android.synthetic.main.item_scrollable.view.*
 import kotlinx.android.synthetic.main.item_view_image.view.*
 import kotlinx.android.synthetic.main.item_view_small_display.view.*
 
@@ -33,6 +39,72 @@ class MainRecyclerViewAdapter(private val context: Context, val list: List<CardG
     private inner class DummyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(position: Int) {
             val string = list[position]
+        }
+    }
+
+    private inner class HC1ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(position: Int) {
+            val cardGroup = list[position]
+
+            if (cardGroup.isScrollable) {
+
+                itemView.recyclerView.adapter = HorizontalRecyclerViewAdapter(
+                    context,
+                    cardGroup.cards,
+                    HC1
+                )
+                itemView.recyclerView.layoutManager = LinearLayoutManager(
+                    context,
+                    LinearLayoutManager.HORIZONTAL,
+                    false
+                )
+                itemView.recyclerView.setHasFixedSize(true)
+                itemView.recyclerView.visibility = View.VISIBLE
+                itemView.ll_card_container.visibility = View.GONE
+
+            } else {
+
+                itemView.recyclerView.visibility = View.GONE
+                itemView.ll_card_container.visibility = View.VISIBLE
+
+                val weightSum = cardGroup.cards.size
+                itemView.ll_card_container.weightSum = weightSum.toFloat()
+                itemView.ll_card_container.removeAllViews()
+
+                for (card in cardGroup.cards) {
+
+                    val smallDisplayCard = LayoutInflater.from(context).inflate(
+                        R.layout.item_view_small_display,
+                        itemView.ll_card_container, false
+                    )
+
+                    val layoutParams = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                    )
+                    layoutParams.setMargins(8.px, 8.px, 8.px, 8.px)
+                    layoutParams.weight = 1.0f
+                    smallDisplayCard.layoutParams = layoutParams
+
+                    itemView.ll_card_container.addView(smallDisplayCard)
+
+                    smallDisplayCard.tv_title.text = card.formattedTitle.text
+                    if (card.formattedDescription != null) {
+                        smallDisplayCard.tv_description.text = card.formattedDescription.text
+                        smallDisplayCard.tv_description.visibility = View.VISIBLE
+                    } else {
+                        smallDisplayCard.tv_description.visibility = View.GONE
+                    }
+                    Glide.with(itemView.context).load(card.icon.imgUrl)
+                        .placeholder(R.drawable.default_icon_background)
+                        .error(R.drawable.default_icon_background)
+                        .into(smallDisplayCard.iv_icon)
+                    if (smallDisplayCard is MaterialCardView && card.bgColor != null)
+                        smallDisplayCard.setCardBackgroundColor(Color.parseColor(card.bgColor))
+
+                }
+            }
         }
     }
 
@@ -65,9 +137,9 @@ class MainRecyclerViewAdapter(private val context: Context, val list: List<CardG
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         when (viewType) {
             HC1 -> {
-                return DummyViewHolder(
+                return HC1ViewHolder(
                     LayoutInflater.from(context).inflate(
-                        R.layout.item_dummy,
+                        R.layout.item_scrollable,
                         parent,
                         false
                     )
@@ -128,7 +200,7 @@ class MainRecyclerViewAdapter(private val context: Context, val list: List<CardG
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (viewTypeMap[list[position].designType]) {
             HC1 -> {
-                (holder as DummyViewHolder).bind(position)
+                (holder as HC1ViewHolder).bind(position)
             }
             HC3 -> {
                 (holder as DummyViewHolder).bind(position)
